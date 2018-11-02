cde61cc75b3945c26b5d539796f2de454b409017
Cristiano Araujo <cca2@cin.ufpe.br>
2018-11-01 23:18:51 -0300

    Initial Commit

---
+++ StudentLearningObjectives/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StudentLearningObjectives
+//
+//  Created by Cristiano Araújo on 01/11/18.
+//  Copyright © 2018 Cristiano Araújo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
