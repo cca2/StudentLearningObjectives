75fc554e9d05a3e842bd7df92d14ebe7346f8e65
Cristiano Araujo <cca2@cin.ufpe.br>
2018-12-03 16:06:29 -0300

    mostrando objetivos selecionados clicando no texto do objetivo

--- StudentLearningObjectives/IntelligentFeedbackController.swift
+++ StudentLearningObjectives/IntelligentFeedbackController.swift
@@ -12,11 +12,27 @@ protocol IntelligentAlertProtocol {
     func displayAlert(message: String) -> Void
 }
 
+class IntelligentElementToDisplay {
+    var student:Student?
+    var objective:StudentLearningObjective? {
+        didSet {
+            let appDelegate = NSApplication.shared.delegate as! AppDelegate
+            let course = appDelegate.selectedCourse
+            self.student = course?.studentsByID[(objective?.studentID)!]
+            print(">>> 500 \\(self.student.name)<<<")
+        }
+    }
+    var message:IntelligentAlertMessage?
+}
+
 class IntelligentFeedbackController: NSPageController {
     var cblSprint:CBLSprint?
     var listOfMatchesByObjective:[(Student, StudentLearningObjective)]!
     var intelligentAlert:IntelligentAlertMessage?
     let appDelegate = NSApplication.shared.delegate as! AppDelegate
+    
+    var elementsToDisplay:[IntelligentElementToDisplay] = []
+    
     @IBOutlet weak var studentMatchByObjectiveList: NSTableView!
     
     @IBOutlet weak var intelligentLabel: NSTextField!
@@ -33,12 +49,21 @@ class IntelligentFeedbackController: NSPageController {
         
         self.studentMatchByObjectiveList.register(NSNib(nibNamed: "StudentMatchByObjectiveCellView", bundle: .main), forIdentifier: NSUserInterfaceItemIdentifier("StudentMatchByObjectiveCellID"))
         
+        self.studentMatchByObjectiveList.register(NSNib(nibNamed: "StudentMatchByObjectiveCellView", bundle: .main), forIdentifier: NSUserInterfaceItemIdentifier("StudentMatchByObjectiveCellID"))
+        
         appDelegate.onObjectiveSelected = {(student, objective) in
             self.listOfMatchesByObjective = self.appDelegate.selectedSprint?.matchStudents(student: student, objective: objective)
-            self.numberOfMatchs.stringValue = String(self.listOfMatchesByObjective.count)
-            self.numberOfMatchs.isHidden = false
-            self.intelligentLabel.isHidden = false
-            self.studentMatchByObjectiveList.reloadData()
+            self.elementsToDisplay = []
+            self.listOfMatchesByObjective.forEach{
+                match in
+                let intelligentSnippet = IntelligentElementToDisplay()
+                intelligentSnippet.objective = match.1
+                self.elementsToDisplay.append(intelligentSnippet)
+                self.numberOfMatchs.stringValue = String(self.listOfMatchesByObjective.count)
+                self.numberOfMatchs.isHidden = false
+                self.intelligentLabel.isHidden = false
+                self.studentMatchByObjectiveList.reloadData()
+            }
         }
         
         let teamSelectedClosure:((Team) -> ())? = {
@@ -46,6 +71,7 @@ class IntelligentFeedbackController: NSPageController {
             self.numberOfMatchs.isHidden = true
             self.intelligentLabel.isHidden = true
             self.listOfMatchesByObjective = []
+            self.elementsToDisplay = []
             self.studentMatchByObjectiveList.reloadData()
         }
         
@@ -56,56 +82,60 @@ class IntelligentFeedbackController: NSPageController {
             self.numberOfMatchs.isHidden = true
             self.intelligentLabel.isHidden = true
             self.listOfMatchesByObjective = []
+            self.elementsToDisplay = []
             self.studentMatchByObjectiveList.reloadData()
         }
         appDelegate.onStudentSelected.append(studentSelectedClosure)
-        
-        let displayAlertClosure:((String) -> ())? = {
-            message in
-            
-        }
     }
 }
 
 extension IntelligentFeedbackController: NSTableViewDataSource {
     func numberOfRows(in tableView: NSTableView) -> Int {
-        if let matches = self.listOfMatchesByObjective {
-            return matches.count
-        }
-//        }else if self.intelligentAlert != nil {
-//            return 1
-//        }
-        return 0
+        return self.elementsToDisplay.count
     }
     
     func tableView(_ tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
-        if let matches = self.listOfMatchesByObjective {
-            let objective = matches[row].1
+        let elementToDisplay = self.elementsToDisplay[row]
+        if let objective = elementToDisplay.objective {
             let fakeField = NSTextField()
             let item = objective.description
             let objectiveDescriptionWidth = CGFloat(262.0)
-            
             fakeField.stringValue = item
             // exactly how you get the text out of your data array depends on how you set it up
-            
             let yourHeight = fakeField.cell!.cellSize(forBounds: NSMakeRect(CGFloat(0.0), CGFloat(0.0), objectiveDescriptionWidth, CGFloat(Float.greatestFiniteMagnitude))).height + 70.0
             return yourHeight
         }else {
             return 50.0
         }
+//        if let matches = self.listOfMatchesByObjective {
+//            let objective = matches[row].1
+//            let fakeField = NSTextField()
+//            let item = objective.description
+//            let objectiveDescriptionWidth = CGFloat(262.0)
+//
+//            fakeField.stringValue = item
+//            // exactly how you get the text out of your data array depends on how you set it up
+//
+//            let yourHeight = fakeField.cell!.cellSize(forBounds: NSMakeRect(CGFloat(0.0), CGFloat(0.0), objectiveDescriptionWidth, CGFloat(Float.greatestFiniteMagnitude))).height + 70.0
+//            return yourHeight
+//        }else {
+//            return 50.0
+//        }
     }
 }
 
 extension IntelligentFeedbackController: NSTableViewDelegate {
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
         if (tableView == self.studentMatchByObjectiveList) {
-            let cellIdentifier = "StudentMatchByObjectiveCellID"
-            
-            if tableColumn == tableView.tableColumns[0] {
-                if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(cellIdentifier), owner: nil) as?  StudentMatchByObjectiveCellView {
-                    cell.studentName.stringValue = self.listOfMatchesByObjective[row].0.name
-                    cell.displayObjective(objective: self.listOfMatchesByObjective[row].1)
-                    return cell
+            let elementToDisplay = self.elementsToDisplay[row]
+            if let objective = elementToDisplay.objective {
+                let cellIdentifier = "StudentMatchByObjectiveCellID"
+                if tableColumn == tableView.tableColumns[0] {
+                    if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(cellIdentifier), owner: nil) as?  StudentMatchByObjectiveCellView {
+                        cell.studentName.stringValue = (elementToDisplay.student?.name)!
+                        cell.displayObjective(objective: objective)
+                        return cell
+                    }
                 }
             }
         }
